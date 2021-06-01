4a729c453548e69c1abe405bf54704b9b0f8ce7e
Ryan@CloudStudio <AtCloudStudio@gmail.com>
2021-05-31 21:44:56 -0400

    Episode 12

---
+++ Scripts/UI/StatsBar.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsBar : MonoBehaviour
+{
+    [SerializeField] Image fillImageBack;
+    [SerializeField] Image fillImageFront;
+    [SerializeField] bool delayFill = true;
+    [SerializeField] float fillDelay = 0.5f;
+    [SerializeField] float fillSpeed = 0.1f;
+
+    float currentFillAmount;
+    protected float targetFillAmount;
+    float t;
+
+    WaitForSeconds waitForDelayFill;
+
+    Coroutine bufferedFillingCoroutine;
+
+    Canvas canvas;
+
+    void Awake()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.worldCamera = Camera.main;
+
+        waitForDelayFill = new WaitForSeconds(fillDelay);
+    }
+
+    public virtual void Initialize(float currentValue, float maxValue)
+    {
+        currentFillAmount = currentValue / maxValue;
+        targetFillAmount = currentFillAmount;
+        fillImageBack.fillAmount = currentFillAmount;
+        fillImageFront.fillAmount = currentFillAmount;
+    }
+
+    public void UpdateStats(float currentValue, float maxValue)
+    {
+        targetFillAmount = currentValue / maxValue;
+
+        if (bufferedFillingCoroutine != null)
+        {
+            StopCoroutine(bufferedFillingCoroutine);
+        }
+
+        if (currentFillAmount > targetFillAmount)
+        {
+            fillImageFront.fillAmount = targetFillAmount;
+            bufferedFillingCoroutine = StartCoroutine(BufferedFillingCoroutine(fillImageBack));
+        }
+
+        if (currentFillAmount < targetFillAmount)
+        {
+            fillImageBack.fillAmount = targetFillAmount;
+            bufferedFillingCoroutine = StartCoroutine(BufferedFillingCoroutine(fillImageFront));
+        }
+    }
+
+    protected virtual IEnumerator BufferedFillingCoroutine(Image image)
+    {
+        if (delayFill)
+        {
+            yield return waitForDelayFill;
+        }
+
+        t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * fillSpeed;
+            currentFillAmount = Mathf.Lerp(currentFillAmount, targetFillAmount, t);
+            image.fillAmount = currentFillAmount;
+
+            yield return null;
+        }
+    }
+}
---
+++ Scripts/UI/StatsBar_HUD.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsBar_HUD : StatsBar
+{
+    [SerializeField] Text percentText;
+
+    void SetPercentText()
+    {
+        percentText.text = Mathf.RoundToInt(targetFillAmount * 100f) + "%";
+    }
+
+    public override void Initialize(float currentValue, float maxValue)
+    {
+        base.Initialize(currentValue, maxValue);
+        SetPercentText();
+    }
+
+    protected override IEnumerator BufferedFillingCoroutine(Image image)
+    {
+        SetPercentText();
+        return base.BufferedFillingCoroutine(image);
+    }
+}
