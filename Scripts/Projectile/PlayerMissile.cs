3be27ede8f4323f6f30acee96252df5b4f2c631d
Ryan@CloudStudio <AtCloudStudio@gmail.com>
2021-09-16 14:51:33 -0400

    Episode 24.2

--- Scripts/Projectile/PlayerMissile.cs
+++ Scripts/Projectile/PlayerMissile.cs
@@ -11,6 +11,13 @@ public class PlayerMissile : PlayerProjectileOverdrive
     [SerializeField] float highSpeed = 25f;
     [SerializeField] float variableSpeedDelay = 0.5f;
 
+    [Header("==== EXPLOSION ====")]
+    [SerializeField] GameObject explosionVFX = null;
+    [SerializeField] AudioData explosionSFX = null;
+    [SerializeField] LayerMask enemyLayerMask = default;
+    [SerializeField] float explosionRadius = 3f;
+    [SerializeField] float explosionDamage = 100f;
+
     WaitForSeconds waitVariableSpeedDelay;
     
     protected override void Awake()
@@ -25,6 +32,31 @@ public class PlayerMissile : PlayerProjectileOverdrive
         StartCoroutine(nameof(VariableSpeedCoroutine));
     }
 
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+        // Spawn a explosion VFX
+        PoolManager.Release(explosionVFX, transform.position);
+        // Play explosion SFX
+        AudioManager.Instance.PlayRandomSFX(explosionSFX);
+        // Enemies within explosion radius take AOE damage
+        var colliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius, enemyLayerMask);
+
+        foreach (var collider in colliders)
+        {
+            if (collider.TryGetComponent<Enemy>(out Enemy enemy))
+            {
+                enemy.TakeDamage(explosionDamage);
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+    
     IEnumerator VariableSpeedCoroutine()
     {
         moveSpeed = lowSpeed;
@@ -38,4 +70,64 @@ public class PlayerMissile : PlayerProjectileOverdrive
             AudioManager.Instance.PlayRandomSFX(targetAcquiredVoice);
         }
     }
+
+    // * AOE Damage Implementation 1
+    // * 范围伤害实现方法1
+    // [SerializeField] Collider2D explosionCollider = null;
+
+    // protected override void OnEnable()
+    // {
+    //     base.OnEnable();
+    //     StartCoroutine(nameof(VariableSpeedCoroutine));
+    //     // Disable this collider when the missile is launched
+    //     // 导弹发射时禁用这个碰撞体
+    //     explosionCollider.enabled = false;
+    // }
+
+    // protected override void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     base.OnCollisionEnter2D(collision);
+    //     // Spawn an explosion VFX
+    //     PoolManager.Release(explosionVFX, collision.GetContact(0).point);
+    //     // Play explosion SFX
+    //     AudioManager.Instance.PlayRandomSFX(explosionSFX);
+        
+    //     // Turn on this collider for explosion range detection
+    //     //启用这个碰撞体检测爆炸范围
+    //     explosionCollider.enabled = true;
+    // }
+
+    // void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     // If there is any enemy within the trigger range, the enemy takes 100 explosion damage
+    //     // 当检测到任何敌人在触发器范围内，则敌人受到100点爆炸伤害
+    //     if (other.TryGetComponent<Enemy>(out Enemy enemy))
+    //     {
+    //         enemy.TakeDamage(100f);
+    //     }
+    // }
+
+    // * AOE Damage Implementation 2
+    // * 范围伤害实现方法2
+    // !Disadvantages: To detect all enemies in the scene, slightly lower efficiency 
+    // !缺点：检测场景中所有的敌人，效率稍低
+    // private void DistanceDetection()
+    // {
+    //     // Loop detection all enemies in current scene
+    //     // 遍历当前场景中所有的敌人
+    //     foreach (var enemyInRange in EnemyManager.Instance.Enemies)
+    //     {
+    //         // If the distance between the enemy and the missile is within the explosion radius (3f)
+    //         // 如果敌人和导弹的距离在爆炸半径(3f)内
+    //         if (Vector2.Distance(transform.position, enemyInRange.transform.position) <= 3f)
+    //         {
+    //             if (enemyInRange.TryGetComponent<Enemy>(out Enemy enemy))
+    //             {
+    //                 // enemy take 100 damage
+    //                 // 则敌人受到100点伤害
+    //                 enemy.TakeDamage(100f);
+    //             }
+    //         }
+    //     }
+    // }
 }
