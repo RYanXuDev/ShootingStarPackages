e201adfd83c44e3bcb710bd2afffcc7318be00d1
RYanIndieDev <AtCloudStudio@gmail.com>
2021-10-04 16:51:48 -0400

    Episode 25.1

--- Scripts/UI/MainMenuUIController.cs
+++ Scripts/UI/MainMenuUIController.cs
@@ -1,30 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenuUIController : MonoBehaviour
 {
-    [SerializeField] Button buttonStartGame;
+    [Header("==== CANVAS ====")]
+    [SerializeField] Canvas mainMenuCanvas;
+
+    [Header("==== BUTTONS ====")]
+    [SerializeField] Button buttonStart;
+    [SerializeField] Button buttonOptions;
+    [SerializeField] Button buttonQuit;
 
     void OnEnable()
     {
-        buttonStartGame.onClick.AddListener(OnStartGameButtonClick);
+        ButtonPressedBehavior.buttonFunctionTable.Add(buttonStart.gameObject.name, OnButtonStartClicked);
+        ButtonPressedBehavior.buttonFunctionTable.Add(buttonOptions.gameObject.name, OnButtonOptionsClicked);
+        ButtonPressedBehavior.buttonFunctionTable.Add(buttonQuit.gameObject.name, OnButtonQuitClicked);
     }
 
     void OnDisable()
     {
-        buttonStartGame.onClick.RemoveAllListeners();
+        ButtonPressedBehavior.buttonFunctionTable.Clear();
     }
 
     void Start()
     {
         Time.timeScale = 1f;
         GameManager.GameState = GameState.Playing;
+        UIInput.Instance.SelectUI(buttonStart);
     }
 
-    void OnStartGameButtonClick()
+    void OnButtonStartClicked()
     {
+        mainMenuCanvas.enabled = false;
         SceneLoader.Instance.LoadGameplayScene();
+    }
+
+    void OnButtonOptionsClicked()
+    {
+        UIInput.Instance.SelectUI(buttonOptions);
+    }
+
+    void OnButtonQuitClicked()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
     }
 }
